f65ad6f49b3c56ca2b8803f13ae2fa5a9d00a537
mhernandezit <mhernandezit@gmail.com>
2019-01-26 16:31:01 -0500

    Comments, snake_case, general linting

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,15 +1,27 @@
-class book:
-	author = ''
-	title = ''
-	def __init__(self, title, author):
-		self.author = author
-		self.title = title
+"""Assignment1_Part2 working with classes """
+
+class Book(object):
+    """ Book class - each book has a title, author """
+    author = ''
+    title = ''
+    def __init__(self, title, author):
+        self.author = author
+        self.title = title
+
+    def set_author(self, author):
+        """Author setter"""
+        self.author = author
+
+    def set_title(self, title):
+        """Title setter"""
+        self.title = title
 
 def display(book):
-	print '"{}, written by {}"'.format(book.title, book.author)
+    """ display function to display a book's title, author """
+    print '"{}, written by {}"'.format(book.title, book.author)
 
 if __name__ == '__main__':
-	Book1 = book("Of Mice and Men","John Steinbeck")
-	Book2 = book("To Kill a Mockingbird","Harper Lee")
-	display(Book1)
-	display(Book2)
+    BOOK1 = Book("Of Mice and Men", "John Steinbeck")
+    BOOK2 = Book("To Kill a Mockingbird", "Harper Lee")
+    display(BOOK1)
+    display(BOOK2)
