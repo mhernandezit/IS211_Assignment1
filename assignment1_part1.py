3cad0a2d7cb1646018283457066bb5a6b1277826
mhernandezit <mhernandezit@gmail.com>
2019-01-26 15:38:46 -0500

    Linting, commenting

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,41 @@
-def listDivide(numbers, divide=2):
-	isdivisible = 0
-	for number in numbers:
-		if number % divide == 0:
-			isdivisible += 1
-	return isdivisible
+#Michael Hernandez
+#IS211 Assignment 1 Part 1
+"""IS211 Assigment 1 part 1 - listDivide and tests"""
 
-class ListDivideException(Exception):
-	pass
+def listDivide(numbers, divide=2):
+    """ This function takes a list of numbers and tests if the elements are divisible
+    by the divide argument
+
+    Args:
+        numbers (list) A list of integers
+        divide (int) The number we will be dividing the list by
+
+    Returns:
+        isdivisible (int) Number of elements that are divisible by divide variable
+
+    """
+    isdivisible = 0
+    for number in numbers:
+        if number % divide == 0:
+            isdivisible += 1
+    return isdivisible
+
+class listDivideException(Exception):
+    """Custom exception class for assignment 1"""
+    pass
 
 def testListDivide():
-	if listDivide([1,2,3,4,5]) != 2: raise ListDivideException 
-	if listDivide([2,4,6,8,10]) != 5: raise ListDivideException
-	if listDivide([30,54,63,98,100],divide=10) != 2: raise ListDivideException
-	if listDivide([]) != 0: raise ListDivideException
-	if listDivide([1,2,3,4,5], 1) != 5: raise ListDivideException
+    """ Unit tests """
+    if listDivide([1, 2, 3, 4, 5]) != 2:
+        raise listDivideException
+    if listDivide([2, 4, 6, 8, 10]) != 5:
+        raise listDivideException
+    if listDivide([30, 54, 63, 98, 100], divide=10) != 2:
+        raise listDivideException
+    if listDivide([]) != 0:
+        raise listDivideException
+    if listDivide([1, 2, 3, 4, 5], 1) != 5:
+        raise listDivideException
 
 if __name__ == '__main__':
-	testListDivide()
+    testListDivide()
